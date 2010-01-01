f5cd2d321b5581e63d79c668f12bb71bb38c5667
jinx <jinx@jinx.(none)>
2009-12-31 21:27:37 -0500

    Fixed the rotation-only problem.  Wrong param args were set.  Thanks to Matt Klein for the bug-spot

--- catkin/src/cwru_base_hydro/cwru_base/src/twist_receiver.py
+++ catkin/src/cwru_base_hydro/cwru_base/src/twist_receiver.py
@@ -39,7 +39,6 @@ class ToCRIO:
         self.outgoingUDP.sendto(packets.make_vector_driver_packet(heading, speed), self.address)
 
     def send_angular_rate_command(self, angular_rate, speed):
-        rospy.loginfo("spd: %s, ang: %s", speed, angular_rate)
         self.outgoingUDP.sendto(packets.make_angular_rate_driver_packet(angular_rate, speed), self.address)
 
     def send_waypoint_command(self, x, y, heading, speed):
@@ -68,15 +67,15 @@ to_crio = ToCRIO()
 def twist_receiver(msg, params):
     toCRIO = params[0]
     push_casters = params[1]
-    speed_limit = params[2]
-    spin_speed_limit = params[3]
+    speed_limit = params[3]
+    spin_speed_limit = params[4]
     multiplier_z = 1 if rl_swap else -1
     multiplier_x = -1 if push_casters else 1
     if abs(msg.angular.z) > spin_speed_limit:
         msg.angular.z = math.copysign(spin_speed_limit, msg.angular.z)
     if abs(msg.linear.x) > speed_limit:
         msg.linear.x = math.copysign(speed_limit, msg.linear.x)
-    rospy.loginfo("spd: %s, ang: %s", multiplier_x*msg.linear.x, multiplier_z*msg.angular.z)
+    rospy.loginfo("spd: %s, ang: %s", msg.linear.x, multiplier_z*msg.angular.z)
     toCRIO.send_angular_rate_command(multiplier_z*msg.angular.z, multiplier_x*msg.linear.x)
 
 def handle_reboot_request(req):
