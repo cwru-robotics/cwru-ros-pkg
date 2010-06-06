2007e0f073a5352bef1c0c82aeed105186e4f096
Ben <bxb107@case.edu>
2010-06-06 09:59:39 -0400

    new thing to test in sim

--- harlie_obstacle_planner/src/planner.py
+++ harlie_obstacle_planner/src/planner.py
@@ -14,13 +14,12 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
 from nav_msgs.msg import GridCells
 
+
 from numpy import *
 from enthought.mayavi import mlab
 from sys import maxint
 from collections import deque
 import time
-#from scipy import weave 
-
 
 #import gc
 #gc.disable()
@@ -31,13 +30,15 @@ import time
 #Remove Globals
 #USE DEQUE
 
+
+
+
 MAP_SIZE = 4000
 #WALL_SIZE = 41
 WALL_SIZE = 31
 BOX = 61
 REDUCE_SIZE=81
 EXTRA = 100
-
 
 #Defines x y and cost
 class Cell:
@@ -47,13 +48,29 @@ class Cell:
 	def __init__(self,X,Y):
 		self.x=X
 		self.y=Y
+
 def isOdd(num):
 	return num & 1 and True or False
+
+def normalDistributionArray(Size,row,factor):
+	dist = ones((Size,Size))
+	if isOdd(Size) == False:
+		print "normalDistributionArray was sent an even number. This will not work"
+	mid = (Size-1)/2
+	for X in xrange(Size):
+		for Y in xrange(Size):
+			x=X-mid
+			y=Y-mid
+			location=sqrt(x*x+y*y)
+			dist[X][Y] = (1/sqrt(2*row*row*3.14))*exp(-1*((location*location)/(2*row*row))) * factor
+	return dist
+
 def isValidXY(x,y):
 	if x>=0 and x<MAP_SIZE:
 		if y>=0 and y<MAP_SIZE:
 			return True
 	return False
+
 def findNeigbors(x,y):
 	neigbors = list()
 	if isValidXY(x+1,y):
@@ -73,31 +90,7 @@ def findNeigbors(x,y):
 	if isValidXY(x-1,y-1):
 		neigbors.append(Cell(x-1,y-1,))
 	return neigbors
-def normalDistributionArray(Size,row,factor):
-	dist = ones((Size,Size))
-	if isOdd(Size) == False:
-		print "normalDistributionArray was sent an even number. This will not work"
-	mid = (Size-1)/2
-	for X in xrange(Size):
-		for Y in xrange(Size):
-			x=X-mid
-			y=Y-mid
-			location=sqrt(x*x+y*y)
-			dist[X][Y] = (1/sqrt(2*row*row*3.14))*exp(-1*((location*location)/(2*row*row))) * factor
-	return dist
-def isEdge(x,y):
-	neigbors = findNeigbors(x,y)
-	if gradCost[x,y] == 0:
-		for cell in neigbors:
-			if gradCost[cell.x,cell.y] != 0:
-				return True
-	return False
-def isInnerEdge(x,y):
-	neigbors = findNeigbors(x,y)
-	if gradCost[x,y] != 0:
-		for cell in neigbors:
-			if gradCost[cell.x,cell.y] == 0:
-				return True
+
 def constrainValue(val):
 	if val < 0:
 		return 0
@@ -105,72 +98,7 @@ def constrainValue(val):
 		return MAP_SIZE
 	return val
 
-def fullCost(x,y):
-	global FullCost
-	#return gradCost[x,y] - wallCost[x,y] - reduceCost[x,y]
-	return FullCost[x,y]
 
-def robotExpand(cell,expandMask,gradCost):
-	cells = deque()
-	neighbors = findNeigbors(cell.x,cell.y)
-	currentVal = fullCost(cell.x,cell.y)
-	appended = False
-	for neighbor in neighbors:
-		value = fullCost(neighbor.x,neighbor.y)
-		if gradCost[neighbor.x,neighbor.y] != 0:
-			if value > currentVal:
-				if expandMask[neighbor.x,neighbor.y]==0:
-					expandMask[neighbor.x,neighbor.y]=1
-					if isInnerEdge(neighbor.x,neighbor.y) == True:
-						cells.append(neighbor)
-						appended = True
-					cells.extend(robotExpand(neighbor,expandMask,gradCost))
-	if appended == False:
-		cells.append(cell)
-	return cells
-
-
-
-def robotAccendFull(expandMask,robot,gradCost):		
-	#Clear expand mask
-	Xmin = constrainValue(robot.x - BOX)
-	Xmax = constrainValue(robot.x + BOX)
-	Ymin = constrainValue(robot.y - BOX)
-	Ymax = constrainValue(robot.y + BOX)
-	m = expandMask[Xmin:Xmax,Ymin:Ymax]
-	m = zeros(m.shape,int)
-	
-	x = robot.x
-	y = robot.y
-	notEdge = True
-	low  = -maxint
-
-	#Sometimes we get stupid and stuck in a local max this might remove this
-	#neighbors = findNeigbors(x,y)
-	#current = fullCost(x,y);
-	#Xlow = x
-	#Ylow = y
-	#for neighbor in neighbors:
-	#	if fullCost(neighbor.x,neighbor.y) < current:
-	#		current = fullCost(neighbor.x,neighbor.y)
-	#		Xlow = neighbor.x
-	#		Ylow = neighbor.y
-
-	#startCell = Cell(Xlow,Ylow)
-	startCell = Cell(x,y)
-
-	possibleEndPoints = robotExpand(startCell,expandMask,gradCost)
-	goal = Cell(0,0)
-	print len(possibleEndPoints)
-
-	for point in possibleEndPoints:
-		if fullCost(point.x,point.y)>low:
-			#print "Cost = ",fullCost(point.x,point.y)
-			low = fullCost(point.x,point.y)
-			goal = point
-	if goal.x == 0 and goal.y == 0:
-		return robot
-	return goal
 
 def split(a,xMin,yMin,xMax,yMax):
 	b=vsplit(a,[xMax])[0]
@@ -186,33 +114,7 @@ def CopyMaskSum (large, mask,x,y):
 	Ycorner = y - mid-1
 	Xopp = mid + x
 	Yopp = mid + y
-
 	large[Xcorner:Xopp,Ycorner:Yopp] = large[Xcorner:Xopp,Ycorner:Yopp]  + mask[:,:] 
-
-
-def expandGradient(cell,walls,gradCost,gradList):
-	neighbors = findNeigbors(cell.x, cell.y)
-	for neighbor in neighbors:
-		if walls[neighbor.x,neighbor.y] ==  0:
-			if gradCost[neighbor.x,neighbor.y] == 0:
-				gradCost[neighbor.x,neighbor.y] = gradCost[cell.x,cell.y] + 1
-				gradList.appendleft(neighbor)
-
-def makeGradient(gradList,robot,walls,gradCost):	
-	Xmin = robot.x - BOX
-	Xmax = robot.x + BOX
-	Ymin = robot.y - BOX
-	Ymax = robot.y + BOX
-	outList = deque()
-	while gradList:	
-		cell = gradList.pop();
-		if cell.x > Xmin and cell.x < Xmax and cell.y > Ymin and Ymax > cell.y:
-			expandGradient(cell,walls,gradCost,gradList)	
-		else:
-			outList.append(cell)
-				
-	return outList
-
 
 def convertFromOdom(value):
 	resolution = .05
@@ -235,138 +137,222 @@ def sendGoal(goalcell):
 	#Fix Theta
 	quaternion = tf.transformations.quaternion_about_axis(0, (0,0,1))
 	goal.target_pose.pose.orientation = Quaternion(*quaternion)
-
 	client.send_goal(goal)
-	#Do we wait for a resulut
-	#client.wait_for_result()
-	#if client.get_state() == GoalStatus.SUCCEEDED:
-    	#	rospy.loginfo("Goal executed successfully")
-    	#else:
-    	#   	rospy.logerr("Could not execute goal for some reason")
+
+class Planner:
+	def __init__(self):
+		#INITALIZE STUFF
+		#wallGrid = normalDistributionArray(WALL_SIZE,15,400)
+		#wallGrid = normalDistributionArray(WALL_SIZE,15,100)
+		#reduceGrid = normalDistributionArray(REDUCE_SIZE,10,10000)
+		self.wallGrid = normalDistributionArray(WALL_SIZE,6,500)
+		self.walls=list()
+		#reduceGrid = normalDistributionArray(REDUCE_SIZE,30,4000) + normalDistributionArray(REDUCE_SIZE,3,120)
+		self.reduceGrid = normalDistributionArray(REDUCE_SIZE,13,4000)
+		self.wallCost = zeros((MAP_SIZE,MAP_SIZE),float)
+		self.gradCost = zeros((MAP_SIZE,MAP_SIZE),float)
+		self.reduceCost = zeros((MAP_SIZE,MAP_SIZE),float)
+		self.FullCost = zeros((MAP_SIZE,MAP_SIZE),float)
+		self.expandMask = zeros((MAP_SIZE,MAP_SIZE),int)
+		self.gradList = deque()
+		self.hasRobot = False
+		#robot = Cell(3488,2000)
+		#robot = Cell(3500,2600)
+
+	def setRobot(self,robot):
+		self.robot = robot
+		if self.hasRobot == False:
+			self.hasRobot = True
+			self.gradCost[robot.x,robot.y]=1
+			self.gradList.append(robot)
+
+	#def isEdge(x,y):
+	#	neigbors = findNeigbors(x,y)
+	#	if self.gradCost[x,y] == 0:
+	#		for cell in neigbors:
+	#			if self.gradCost[cell.x,cell.y] != 0:
+	#				return True
+	#	return False
+
+	def isInnerEdge(self,x,y):
+		neigbors = findNeigbors(x,y)
+		if self.gradCost[x,y] != 0:
+			for cell in neigbors:
+				if self.gradCost[cell.x,cell.y] == 0:
+					return True
+
+	#def expandToEdge(celltoSet):
+	#	global gradCost
+	#	global walls
+	#	neighbors = findNeigbors(celltoSet.x,celltoSet.y)
+	#	low = maxint
+	#	for cell in neighbors:
+	#		if gradCost[cell.x,cell.y]<low:
+	#			if gradCost[cell.x,cell.y] != 0:
+	#				low = gradCost[cell.x,cell.y]
+	#	gradCost[celltoSet.x,celltoSet.y] = low + 1
+
+	def fullCost(self,x,y):
+		#return gradCost[x,y] - wallCost[x,y] - reduceCost[x,y]
+		return self.FullCost[x,y]
+
+	def robotExpand(self,cell):
+		cells = deque()
+		neighbors = findNeigbors(cell.x,cell.y)
+		currentVal = self.fullCost(cell.x,cell.y)
+		appended = False
+		for neighbor in neighbors:
+			value = self.fullCost(neighbor.x,neighbor.y)
+			if self.gradCost[neighbor.x,neighbor.y] != 0:
+				if value > currentVal:
+					if self.expandMask[neighbor.x,neighbor.y]==0:
+						self.expandMask[neighbor.x,neighbor.y]=1
+						if self.isInnerEdge(neighbor.x,neighbor.y) == True:
+							cells.append(neighbor)
+							appended = True
+						cells.extend(self.robotExpand(neighbor))
+		if appended == False:
+			cells.append(cell)
+		return cells
 
 
-global gradList
-global robot
-global gradCost
-global reduceCost
-global initalized
-global FullCost
-initalized = False
-hasOdom = False
-robot = Cell(23,23)
-#INITALIZE STUFF
-wallGrid = normalDistributionArray(WALL_SIZE,6,500)
-#reduceGrid = normalDistributionArray(REDUCE_SIZE,30,4000) + normalDistributionArray(REDUCE_SIZE,3,120)
-reduceGrid = normalDistributionArray(REDUCE_SIZE,15,4000)
-wallCost = zeros((MAP_SIZE,MAP_SIZE),float)
-isWall = zeros((MAP_SIZE,MAP_SIZE),int)
-gradCost = zeros((MAP_SIZE,MAP_SIZE),float)
-reduceCost = zeros((MAP_SIZE,MAP_SIZE),float)
-FullCost = zeros((MAP_SIZE,MAP_SIZE),float)
-expandMask = zeros((MAP_SIZE,MAP_SIZE),int)
-gradList = deque()
-#robot = Cell(3488,2000)
-#robot = Cell(3500,2600)
-robot = Cell(3500,2400)
-gradCost[robot.x,robot.y]=1
-gradList.append(robot)
 
-def setObstacles(walls):
-	global robot
-	global gradList
-	global initalized
-	global gradCost
-	global reduceCost
-	global FullCost
-	if hasOdom == True:
-		initalized = True
-	if initalized != False:
-			
-		print "Robot x = ", robot.x , " y = ", robot.y
-		start = time.time()
-		Xmin = constrainValue(robot.x - BOX-WALL_SIZE/2)
-		Xmax = constrainValue(robot.x + BOX+WALL_SIZE/2)
-		Ymin = constrainValue(robot.y - BOX-WALL_SIZE/2)
-		Ymax = constrainValue(robot.y + BOX+WALL_SIZE/2)	
-		m = wallCost[Xmin:Xmax,Ymin:Ymax]
-		m = zeros(m.shape,float)
-		isWall[Xmin:Xmax,Ymin:Ymax]=zeros(m.shape,int)
-		for point in walls.cells:
-			wallX = convertFromOdom(point.x)
-			wallY = convertFromOdom(point.y)
-			if wallX < Xmax and wallX > Xmin and wallY>Ymin and wallY<Ymax:
-				CopyMaskSum(wallCost,wallGrid,wallX,wallY)
-				isWall[wallX,wallY]=1
+	def robotAccendFull(self):	
+		#make fullCost
+		Xmin = constrainValue(self.robot.x - BOX)
+		Xmax = constrainValue(self.robot.x + BOX)
+		Ymin = constrainValue(self.robot.y - BOX)
+		Ymax = constrainValue(self.robot.y + BOX)
+		self.FullCost[Xmin:Xmax,Ymin:Ymax] =  self.gradCost[Xmin:Xmax,Ymin:Ymax] - self.wallCost[Xmin:Xmax,Ymin:Ymax] - self.reduceCost[Xmin:Xmax,Ymin:Ymax]
 
+
+		#Clear expand mask
+		m = self.expandMask[Xmin:Xmax,Ymin:Ymax]
+		m = zeros(m.shape,int)
 	
-		print "Wall Cost " ,  time.time() - start
-		wallTime = time.time() - start
+		x = self.robot.x
+		y = self.robot.y
+		low  = -maxint
 
-		start = time.time()
-		gradList = makeGradient(gradList,robot,isWall,gradCost)
-		print "Gradient " ,  time.time() - start
-		gradTime = time.time()-start
-		FUL = BOX+EXTRA
-		#l = mlab.surf(split(gradCost,robot.x-BOX,robot.y-BOX,robot.x+BOX,robot.y+BOX))
+		#Sometimes we get stupid and stuck in a local max this might remove this
+		#neighbors = findNeigbors(x,y)
+		#current = fullCost(x,y);
+		#Xlow = x
+		#Ylow = y
+		#for neighbor in neighbors:
+		#	if fullCost(neighbor.x,neighbor.y) < current:
+		#		current = fullCost(neighbor.x,neighbor.y)
+		#		Xlow = neighbor.x
+		#		Ylow = neighbor.y
 
-		CopyMaskSum(reduceCost,reduceGrid,robot.x,robot.y)
-		start = time.time()
+		#startCell = Cell(Xlow,Ylow)
+		startCell = Cell(x,y)
+
+		possibleEndPoints = self.robotExpand(startCell)
+		goal = Cell(0,0)
+		print len(possibleEndPoints)
+
+		for point in possibleEndPoints:
+			if self.fullCost(point.x,point.y)>low:
+				low = self.fullCost(point.x,point.y)
+				goal = point
+		if goal.x == 0 and goal.y == 0:
+			return self.robot
+		return goal
+
+	def makeWallCost(self):
+		Xmin = constrainValue(self.robot.x - BOX)
+		Xmax = constrainValue(self.robot.x + BOX)
+		Ymin = constrainValue(self.robot.y - BOX)
+		Ymax = constrainValue(self.robot.y + BOX)
+		m = self.wallCost[Xmin:Xmax,Ymin:Ymax]
+		m = zeros(m.shape,int)
+		wallMid = (WALL_SIZE-1)/2
+		for wall in walls:
+			if wall.x>Xmin and wall.x < Xmax and wall.y >Ymin and wall.y < Ymax:
+				CopyMaskSum(self.wallCost,self.wallGrid,wall.x,wall.y)
+
+	def expandGradient(self,cell):
+		neighbors = findNeigbors(cell.x, cell.y)
+		for neighbor in neighbors:
+			if self.walls[neighbor.x,neighbor.y] ==  0:
+				if self.gradCost[neighbor.x,neighbor.y] == 0:
+					self.gradCost[neighbor.x,neighbor.y] = self.gradCost[cell.x,cell.y] + 1
+					self.gradList.appendleft(neighbor)
+
+	def makeGradient(self):	
+		Xmin = constrainValue(self.robot.x - BOX)
+		Xmax = constrainValue(self.robot.x + BOX)
+		Ymin = constrainValue(self.robot.y - BOX)
+		Ymax = constrainValue(self.robot.y + BOX)
+		outList = deque()
+		while self.gradList:	
+			cell = self.gradList.pop();
+			if cell.x > Xmin and cell.x < Xmax and cell.y > Ymin and Ymax > cell.y:
+				self.expandGradient(cell)	
+			else:
+				outList.append(cell)
+				
+		self.gradList=outList
 
 
-		Xmin = robot.x - BOX
-		Xmax = robot.x + BOX
-		Ymin = robot.y - BOX
-		Ymax = robot.y + BOX
-		start = time.time()	
-		FullCost[Xmin:Xmax,Ymin:Ymax] =  gradCost[Xmin:Xmax,Ymin:Ymax] - wallCost[Xmin:Xmax,Ymin:Ymax] - reduceCost[Xmin:Xmax,Ymin:Ymax]
-		robot = robotAccendFull(expandMask,robot,gradCost)
-		print "Accending " ,  time.time() - start
-		accendTime = time.time()-start
-		sendGoal(robot)
-		print "Total time = ",wallTime+gradTime+accendTime
+	def runUpdatePlanner(self):
+		if self.hasRobot == True:
+			start = time.time()
+			self.makeWallCost()
+			print "Wall Cost " ,  time.time() - start
+			wallTime = time.time() - start
+	
 
-		print "Robot X,Y"
-		print robot.x , robot.y
-		data=split(FullCost,robot.x-FUL,robot.y-FUL,robot.x+FUL,robot.y+FUL)
-		print data.shape
-		l = mlab.surf(data)
-		mlab.show()
+			start = time.time()
+			self.makeGradient()
+			print "Gradient " ,  time.time() - start
+			gradTime = time.time()-start
+		
+			CopyMaskSum(self.reduceCost,self.reduceGrid,self.robot.x,self.robot.y)
 
-
-
-
-
-
+			start = time.time()
+	
+			goal = self.robotAccendFull()
+			print "Accending " ,  time.time() - start
+			accendTime = time.time()-start
+			print "Total time = ",wallTime+gradTime+accendTime
+			return goal
 
 
 
+#Might need a mutex around the planner.walls value!!!!!!!
+def setObstacles(walls,planner):
+	cells = list()	
+	for point in walls.cells:
+		wallX = convertFromOdom(point.x)
+		wallY = convertFromOdom(point.y)
+		cells.append(Cell(wallX,wallY))
+	planner.walls=cells
 
-def setRobotPose(Odom):
-	global hasOdom
+
+def setRobotPose(Odom,planner):
+	robot = Cell(0,0)
 	robot.y = convertFromOdom(Odom.pose.pose.position.y)	
 	robot.x = convertFromOdom(Odom.pose.pose.position.x)
-	hasOdom=True
+	planner.SetRobot(robot)
 
-
-#def main():
-#	rospy.Subscriber("/odom", Odometry, setRobotPose)
-#	rospy.Subscriber("/move_base/global_costmap/inflated_obstacles", GridCells, setObstacles)		
-#	rospy.spin()
-
-rospy.init_node('harlie_obstacle_planner')
-rospy.Subscriber("/odom", Odometry, setRobotPose)
-rospy.Subscriber("/move_base/global_costmap/inflated_obstacles", GridCells, setObstacles)		
-rospy.spin()
-
-
-
+def main():
+	planner = Planner()
+	rospy.init_node('harlie_obstacle_planner')
+	rospy.Subscriber("/odom", Odometry, setRobotPose,planner)
+	rospy.Subscriber("/move_base/global_costmap/inflated_obstacles", GridCells, setObstacles,planner)		
+	rate = rospy.client.Rate(.5)
+	while not rospy.is_shutdown():
+		print time.time()
+		goal = planner.runUpdatePlanner()	
+		if goal:
+			sendGoal(goal)
+			#Show some data
+			data=split(planner.FullCost,planner.robot.x-FUL,planner.robot.y-FUL,planner.robot.x+FUL,planner.robot.y+FUL)
+			l = mlab.surf(data)
+			mlab.show()
+		rate.sleep()
 if __name__ == '__main__':
     rospy.init_node('harlie_obstacle_planner')
-    #main(rospy.get_param('~filename'))
     main()
-
-
-
-
-
-
