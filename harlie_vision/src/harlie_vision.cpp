3439edefd6ad645522b2ae8e4bdf8ba3707ec537
Eric Perko <eric@mandark.(none)>
2010-06-06 09:58:08 -0400

    More HARLIE vision changes

--- harlie_vision/src/harlie_vision.cpp
+++ harlie_vision/src/harlie_vision.cpp
@@ -27,7 +27,7 @@ HarlieVision::HarlieVision() : it_(nh_) {
 	image_subscriber = it_.subscribe("image_rect_color", 1, &HarlieVision::image_callback, this);
 	image_publisher = it_.advertise("plan_view", 1);
 
-	H = (cv::Mat_<double>(3,3) << -6.5210, -1.0700e-15, 0, -0.0959, -1.5842, -0.0223, 1.9347e3, 3.489203, 1); 
+	H = (cv::Mat_<double>(3,3) << -2.05132604, -1.09442568, 968.34979248, -2.18138890e-03, -2.11626363, 604.70727539, 1.61322518e-04, -3.26651102e-03, 1.0); 
 }
 
 void HarlieVision::image_callback(const sensor_msgs::ImageConstPtr& msg) {
@@ -37,15 +37,14 @@ void HarlieVision::image_callback(const sensor_msgs::ImageConstPtr& msg) {
 	try
 	{
 		image = cv::Mat(bridge.imgMsgToCv(msg, "bgr8"));
-
 	}
 	catch (sensor_msgs::CvBridgeException& e)
 	{
 		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
 	}
 	try {
-		cv::warpPerspective(image, output, H, cv::Size(1287, 1281), CV_INTER_LINEAR | CV_WARP_INVERSE_MAP | CV_WARP_FILL_OUTLIERS);
-		cv::imshow("view", output);
+		cv::warpPerspective(image, output, H, cv::Size(600,600), CV_INTER_LINEAR | CV_WARP_INVERSE_MAP | CV_WARP_FILL_OUTLIERS);
+		//cv::imshow("view", output);
 		IplImage temp = output;
 		image_publisher.publish(bridge.cvToImgMsg(&temp, "bgr8"));
 	}
