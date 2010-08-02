1ae0b95395ee04ef8fd55cbead382285a032342d
Jesse <jesse@ubuntu.ubuntu-domain>
2010-08-02 15:28:34 -0400

    changed vision stuff

--- cwru_tour_guide/tour_guide_vision/src/vision_analyzer.cpp
+++ cwru_tour_guide/tour_guide_vision/src/vision_analyzer.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <cv_bridge/CvBridge.h>
@@ -38,6 +39,9 @@ class VisionAnalyzer {
 		ros::NodeHandle priv_nh_;  
 		image_transport::ImageTransport it_;
 		image_transport::Subscriber image_subscriber;
+		image_transport::Publisher calib_pub_;
+		image_transport::Publisher analyzed_pub_;
+
 };
 
 VisionAnalyzer::VisionAnalyzer() : it_(nh_), priv_nh_("~"){
@@ -68,6 +72,11 @@ VisionAnalyzer::VisionAnalyzer() : it_(nh_), priv_nh_("~"){
 	}
 	
 	image_subscriber= it_.subscribe("image_rect_color", 1, &VisionAnalyzer::image_callback, this);
+	
+	analyzed_pub_= it_.advertise("thresholded_image", 1);
+	calib_pub_= it_.advertise("calibrated_image", 1);
+
+	
 }
 
 void VisionAnalyzer::warp(){
@@ -98,25 +107,37 @@ void VisionAnalyzer::image_callback(const sensor_msgs::ImageConstPtr& msg) {
 	}
 	catch (sensor_msgs::CvBridgeException& e)
 	{
-		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+		ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
 		return;
 	}
 	
 	if(image_rect!=NULL) {
-    cvShowImage("rectified image",image_rect);
+ //   cvShowImage("rectified image",image_rect);
     warp();
-    cvShowImage("calib image",calibrated);
+ //   cvShowImage("calib image",calibrated);
     
     cvThreshold(calibrated,output_image,threshold,255, CV_THRESH_BINARY);
     
-    cvShowImage("threshold",output_image);
+ //   cvShowImage("threshold",output_image);
     
-    cvCanny(output_image,output_image,canny_threshold1,canny_threshold2,canny_aperture_size);
+  //  cvCanny(output_image,output_image,canny_threshold1,canny_threshold2,canny_aperture_size);
     
     
-    cvShowImage("edges",output_image);
-    
-
+   // cvShowImage("edges",output_image);
+    try{
+      sensor_msgs::Image output_image_cvim =*bridge.cvToImgMsg(output_image, "mono8");
+      output_image_cvim.header.stamp=msg->header.stamp;
+      analyzed_pub_.publish(output_image_cvim);
+      
+      sensor_msgs::Image calibrated_image_cvim =*bridge.cvToImgMsg(calibrated, "mono8");
+      calibrated_image_cvim.header.stamp=msg->header.stamp;
+      calib_pub_.publish(calibrated_image_cvim);
+	  }
+	  catch (sensor_msgs::CvBridgeException& e)
+	  {
+		  ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
+		  return;
+	  }
 	 // printf("displaying image\n");
   }else{
    // printf("null image_rect\n");
@@ -129,15 +150,15 @@ int main(int argc, char *argv[]){
  	ros::init(argc, argv, "vision_analyzer");
 	VisionAnalyzer calibrator;
 	cvStartWindowThread();
-  cvNamedWindow("rectified image");
+/*  cvNamedWindow("rectified image");
   cvNamedWindow("calib image");
   cvNamedWindow("threshold");
-  cvNamedWindow("edges");
+  cvNamedWindow("edges");*/
   ros::spin();
-	cvDestroyWindow("rectified image");
+/*	cvDestroyWindow("rectified image");
 	cvDestroyWindow("calib image");
   cvDestroyWindow("threshold");
-  cvDestroyWindow("edges");
+  cvDestroyWindow("edges");*/
   return 0;
 }
 
