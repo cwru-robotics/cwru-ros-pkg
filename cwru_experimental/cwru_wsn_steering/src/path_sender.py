9f6660c63eac17c073c4aee1f410c8c43fdcd090
Eric Perko <wisesage5001@gmail.com>
2010-08-11 01:43:31 -0400

    Updated the path sender to deal wiht door commands

--- cwru_experimental/cwru_wsn_steering/src/path_sender.py
+++ cwru_experimental/cwru_wsn_steering/src/path_sender.py
@@ -3,6 +3,7 @@ import roslib
 roslib.load_manifest('cwru_wsn_steering')
 import rospy
 
+from std_msgs.msg import String
 from cwru_wsn_steering.msg import Path
 from cwru_wsn_steering.msg import PathSegment
 
@@ -68,23 +69,91 @@ def makeDummyPaths():
 	p.tangentAng = 0.7121
 	p.rho = 1.0
 	p.length = 1.57
-	p.vDes = 0.1
+	p.vDes = 0.3
 	p.accel = 0.05
 	p2.segs.append(p)
 	paths['elevator'] = p2
+
+	p3 = Path()
+	p = PathSegment()
+	p.frame_id = "map"
+	p.segType = 2
+	p.xRef = 8.7
+	p.yRef = 16.3
+	p.tangentAng = 2.283
+	p.rho = 100.0
+	p.length = 0.0157 
+	p.vDes = 0.003
+	p.accel = 0.1
+	p3.segs.append(p)
+
+	p = PathSegment()
+	p.frame_id = "map"
+	p.segType = 1
+	p.xRef = 8.74
+	p.yRef = 16.31
+	p.tangentAng = -2.2740
+	p.rho = 0.0
+	p.length = 4.40
+	p.vDes = 0.5
+	p.accel = 0.1
+	p3.segs.append(p)
+
+	p = PathSegment()
+	p.frame_id = "map"
+	p.segType = 2
+	p.xRef =5.31
+	p.yRef = 13.46
+	p.tangentAng = -2.274
+	p.rho = -1.30
+	p.length = 1.21
+	p.vDes = 0.4
+	p.accel = 0.1
+	p3.segs.append(p)
+
+
+	p = PathSegment()
+	p.frame_id = "map"
+	p.segType = 1
+	p.xRef = 4.82
+	p.yRef = 12.75
+	p.tangentAng = 2.35
+	p.rho = 0.0
+	p.length = 5.70
+	p.vDes = 0.5
+	p.accel = 0.1
+	p3.segs.append(p)
+
+	p = PathSegment()
+	p.frame_id = "map"
+	p.segType = 2
+	p.xRef = 0.81
+	p.yRef = 16.80
+	p.tangentAng = 2.350
+	p.rho = -100.0
+	p.length = 0.0157
+	p.vDes = 0.004
+	p.accel = 0.1
+	p3.segs.append(p)
+	paths['bathroom'] = p3
+	
 	return paths
 
 class PathSender:
 	def __init__(self):
 		self.paths = makeDummyPaths()
+		self.halt_path = Path()
 		self.path_pub = rospy.Publisher('desired_path', Path)
+		self.command_sub = rospy.Subscriber('chatter', String, self.handle_chatter)
 
-	def run(self):
-		import pdb
-		pdb.set_trace()
+	def handle_chatter(self, msg):
+		if 'open' in msg.data or 'close' in msg.data:
+			pass
+		else:
+		    path = self.paths.get(msg.data, self.halt_path)
+		    self.path_pub.publish(path)
 
 if __name__ == "__main__":
 	rospy.init_node("path_sender")
 	ps = PathSender()
-	ps.run()
 	rospy.spin()
