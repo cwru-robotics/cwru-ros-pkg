c240b5fd1a7a417e8b3cbcfbd6e559f428c821c0
Eric Perko <wisesage5001@gmail.com>
2010-09-21 21:33:57 -0400

    Updated the map sensor to transform the point clouds

--- cwru_semi_stable/cwru_nav/src/map_as_sensor.cpp
+++ cwru_semi_stable/cwru_nav/src/map_as_sensor.cpp
@@ -2,6 +2,7 @@
 #include <sensor_msgs/PointCloud.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/Point32.h>
+#include <tf/transform_listener.h>
 #include <string>
 #include <list>
 
@@ -14,95 +15,108 @@ bool updated_map;
 
 void mapOccupancyGridCallback(const  nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-  points.clear();
-  recieved_map=true;
-  updated_map=true;
-  frame_id=msg->header.frame_id;
-  geometry_msgs::Point32 p;
-  for(unsigned int m=0;m<msg->info.height;m++){
-    for(unsigned int n=0;n<msg->info.width;n++){
-    //currently 100 is the only value used for obsticles in the map
-      if(msg->data[m*msg->info.width+n]==100){
-          p.x=n*msg->info.resolution+msg->info.origin.position.x;
-          p.y=m*msg->info.resolution+msg->info.origin.position.y;
-          points.push_back(p);
-      }
-    }
-  }
+	points.clear();
+	recieved_map=true;
+	updated_map=true;
+	frame_id=msg->header.frame_id;
+	geometry_msgs::Point32 p;
+	for(unsigned int m=0;m<msg->info.height;m++){
+		for(unsigned int n=0;n<msg->info.width;n++){
+			//currently 100 is the only value used for obsticles in the map
+			if(msg->data[m*msg->info.width+n]==100){
+				p.x=n*msg->info.resolution+msg->info.origin.position.x;
+				p.y=m*msg->info.resolution+msg->info.origin.position.y;
+				points.push_back(p);
+			}
+		}
+	}
 }
 
 
 int main(int argc, char *argv[]){
-  //boolean switch for if we have ever recieved a map
-  recieved_map=false;
-  //boolean switch that triggers every time the map is updated
-  updated_map=false;
-  
-  
-  //intensity of all points in the cloud
-  double intensity=100;
-  //rate in Hz the sensor outputs its data
-  int sensor_rate=5;
-  //height of the sensor in meters
-  double height=8.;
-  //name of the channel the points are in, for the cloud
-  std::string channel_name="intensity";
+	//boolean switch for if we have ever recieved a map
+	recieved_map=false;
+	//boolean switch that triggers every time the map is updated
+	updated_map=false;
 
-  //start node
- 	ros::init(argc, argv, "map_as_sensor");
- 	ros::NodeHandle n;
- 	ros::NodeHandle priv_nh_("~"); 
- 	
- 	// parameters for the node
- 	priv_nh_.param("intensity", intensity,100.);
+
+	//intensity of all points in the cloud
+	double intensity=100;
+	//rate in Hz the sensor outputs its data
+	int sensor_rate=5;
+	//height of the sensor in meters
+	double height=8.;
+	//name of the channel the points are in, for the cloud
+	std::string channel_name="intensity";
+
+	//name of the tf frame to put the point cloud in so that the origin of the point cloud will be within the rolling window for the local costmap
+	std::string target_frame="base_link";
+
+	//start node
+	ros::init(argc, argv, "map_as_sensor");
+	ros::NodeHandle n;
+	ros::NodeHandle priv_nh_("~"); 
+
+	// parameters for the node
+	priv_nh_.param("intensity", intensity,100.);
 	priv_nh_.param("sensor_rate",sensor_rate, 1);
 	priv_nh_.param("channel_name",channel_name, std::string("intensity"));
-	priv_nh_.param("height",height, 8.);
- 	
-  ros::Subscriber sub = n.subscribe("input_map", 1, &mapOccupancyGridCallback);
- 
-  ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud>("map_cloud",1);
+	priv_nh_.param("height",height, 1.9);
+	priv_nh_.param("target_frame", target_frame, std::string("base_link"));
 
-  sensor_msgs::PointCloud * sensor_points=NULL;
-    //continuously publish the point cloud at a set rate
-  ros::Rate r(sensor_rate);
-  
-  
-  while(n.ok()){
-    if(recieved_map){
-      if(updated_map){
-        updated_map=false;
-        
-        if(sensor_points!=NULL){ delete(sensor_points);}
-        
-        sensor_points=NULL;
-        sensor_points=new sensor_msgs::PointCloud;
-        sensor_points->set_points_size(points.size());
-        
-        sensor_points->set_channels_size(1);
-        sensor_points->channels[0].name = channel_name;
-        sensor_points->channels[0].set_values_size(points.size());
-        
-        sensor_points->header.frame_id =frame_id;
-        //for everything in list points, set the x and y value to the right stuff, and set intensity to intensity
-        int point_number=0;
-        for(std::list<geometry_msgs::Point32>::const_iterator iterator = points.begin(), end = points.end(); iterator != end; ++iterator){
-          sensor_points->points[point_number].x = (*iterator).x;
-          sensor_points->points[point_number].y = (*iterator).y;
-          sensor_points->points[point_number].z = height;
-          sensor_points->channels[0].values[point_number] = (float)intensity;
-         ++point_number;
-        }
-      }
-      
-      sensor_points->header.stamp = ros::Time::now();
-      cloud_pub.publish(*sensor_points);
-    }
-    
-    ros::spinOnce();
-    r.sleep();
-  }
+	ros::Subscriber sub = n.subscribe("input_map", 1, &mapOccupancyGridCallback);
 
-  return 0;
+	ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud>("map_cloud",1);
+
+	sensor_msgs::PointCloud * sensor_points=NULL;
+	//continuously publish the point cloud at a set rate
+	ros::Rate r(sensor_rate);
+
+	ros::Rate sleep_rate(1.0);
+	while(ros::ok() && !recieved_map) {
+	   ROS_INFO("Waiting to receive a map before looking up the transform information"); 
+	   sleep_rate.sleep();
+	}
+	tf::TransformListener listener;
+	listener.waitForTransform(target_frame, frame_id, ros::Time::now(), ros::Duration(10));
+	sensor_msgs::PointCloud temp_out;
+
+	while(n.ok()){
+		if(recieved_map){
+			if(updated_map){
+				updated_map=false;
+
+				if(sensor_points!=NULL){ delete(sensor_points);}
+
+				sensor_points=NULL;
+				sensor_points=new sensor_msgs::PointCloud;
+				sensor_points->set_points_size(points.size());
+
+				sensor_points->set_channels_size(1);
+				sensor_points->channels[0].name = channel_name;
+				sensor_points->channels[0].set_values_size(points.size());
+
+				sensor_points->header.frame_id =frame_id;
+				//for everything in list points, set the x and y value to the right stuff, and set intensity to intensity
+				int point_number=0;
+				for(std::list<geometry_msgs::Point32>::const_iterator iterator = points.begin(), end = points.end(); iterator != end; ++iterator){
+					sensor_points->points[point_number].x = (*iterator).x;
+					sensor_points->points[point_number].y = (*iterator).y;
+					sensor_points->points[point_number].z = height;
+					sensor_points->channels[0].values[point_number] = (float)intensity;
+					++point_number;
+				}
+			}
+
+			sensor_points->header.stamp = ros::Time::now();
+			listener.transformPointCloud(target_frame, *sensor_points, temp_out);
+			cloud_pub.publish(temp_out);
+		}
+
+		ros::spinOnce();
+		r.sleep();
+	}
+
+	return 0;
 }
 
