536d52f7fed292c30da80e6ee9a053983122dc11
Eric Perko <wisesage5001@gmail.com>
2010-09-21 20:48:42 -0400

    Added a bash script to run some nice plotters for the steering stuff

---
+++ cwru_experimental/cwru_wsn_steering/steering_plots.bash
@@ -0,0 +1,3 @@
+rxplot /cmd_vel/linear/x,/odom/twist/twist/linear/x /cmd_vel/angular/z,/odom/twist/twist/angular/z &
+rxplot /idealState/x,/flipped_pose/x /idealState/y,/flipped_pose/y /idealState/theta,/flipped_pose/theta &
+wait
