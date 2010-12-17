a54ea0a176d19d540ff2e56281f9ae6510e32ae0
Chad Rockey <chadrockey@gmail.com>
2010-12-16 21:59:11 -0500

    Things should work and be at least usable at this point.

--- cwru_experimental/cwru_kalibrater/src/cwru_ekf.py
+++ cwru_experimental/cwru_kalibrater/src/cwru_ekf.py
@@ -27,47 +27,108 @@ from geometry_msgs.msg import PoseWithCovariance
 from geometry_msgs.msg import Quaternion
 from cwru_base.msg import cRIOSensors
 
-class EKF:
-  def __init__(self):
+class EKF:    
+  def __init__(self, gX):
+    if gX is None:
+      # Read from YAML
+      # Kalman process noise
+      qxy = float(rospy.get_param('/cwru_ekf/CWRU_EKF/qXY', '1e-10'))
+      qth = float(rospy.get_param('/cwru_ekf/CWRU_EKF/qTh', '.1e-10'))
+      qv = float(rospy.get_param('/cwru_ekf/CWRU_EKF/qV', '1.10579509e+00'))
+      qw = float(rospy.get_param('/cwru_ekf/CWRU_EKF/qW', '1.50573452e+00'))
+      qb = float(rospy.get_param('/cwru_ekf/CWRU_EKF/qBias', '1.14688434e-06'))
+      q = array((qxy, qxy, qth, qv, qw, qb))
+      self.Q = diagflat(q)
+      
+      # Model constants
+      self.lec = float(rospy.get_param('/cwru_ekf/CWRU_EKF/lec', '8.57812704e-04'))
+      self.rec = float(rospy.get_param('/cwru_ekf/CWRU_EKF/rec', '8.53890657e-04'))
+      self.lmec = float(rospy.get_param('/cwru_ekf/CWRU_EKF/lmec', '0.000057187'))
+      self.rmec = float(rospy.get_param('/cwru_ekf/CWRU_EKF/rmec', '0.000056932'))
+      self.yc = float(rospy.get_param('/cwru_ekf/CWRU_EKF/yc', '1.25301732e-02'))
+      self.track = float(rospy.get_param('/cwru_ekf/CWRU_EKF/track', '0.561975'))
+      self.dt = float(rospy.get_param('/cwru_ekf/CWRU_EKF/dt', '0.02'))
+      
+      encVc = float(rospy.get_param('/cwru_ekf/CWRU_EKF/encVc', '8.79895355e-06'))
+      encVl = float(rospy.get_param('/cwru_ekf/CWRU_EKF/encVl', '0.0'))
+      encVs = float(rospy.get_param('/cwru_ekf/CWRU_EKF/encVs', '0.0'))
+      yawVc = float(rospy.get_param('/cwru_ekf/CWRU_EKF/yawVc', '1.15810749e-02'))
+      yawVl = float(rospy.get_param('/cwru_ekf/CWRU_EKF/yawVl', '0.0'))
+      yawVs = float(rospy.get_param('/cwru_ekf/CWRU_EKF/yawVs', '0.0'))
+      
+      self.runEnc = bool(rospy.get_param('/cwru_ekf/CWRU_EKF/runEnc', 'true'))
+      self.runYaw = bool(rospy.get_param('/cwru_ekf/CWRU_EKF/runYaw', 'true'))
+      
+      # Sensor variances
+      self.encV = (encVc, encVl, encVs) # Constant, "linear", "square"
+      self.yawV = (yawVc, yawVl, yawVs) # Constant, "linear", "square"
+      
+      self.publish = 1
+    else:
+      # gX is: qV qW qB encVc encVl yawVc yawVl lec rec lmec rmec yc track
+    
+      qV = gX[0]
+      qW = gX[1]
+      qB = gX[2]
+      
+      encVc = gX[3]
+      encVl = gX[4]
+      yawVc = gX[5]
+      yawVl = gX[6]
+      
+      self.lec = gX[7]
+      self.rec = gX[8]
+      self.lmec = gX[9]
+      self.rmec = gX[10]
+      self.yc = gX[11]
+      self.track = gX[12]
+
+      # Kalman process noise
+      q = array((1e-8, 1e-8, 1e-8, qV, qW, qB))
+      self.Q = diagflat(q)
+      
+      # Sensor variances
+      self.encV = (encVc, encVl, 0.0) # Constant, "linear", "square"
+      self.yawV = (yawVc, yawVl, 0.0) # Constant, "linear", "square"
+
+      self.runEnc = True
+      self.runYaw = True
+      
+      # Model constants
+      self.dt = 1.0/50.0
+      
+      self.publish = 0
+
+    self.initCommon()
+
+  def initCommon(self):
     # Kalman state variables
     self.x = zeros((6,1)) # Kalman state [x; y; th; v; w; Vbias]
     self.P = eye(6) # Covariance matrix
-
-    # Model constants
-    self.lec = 0.00087589
-    self.rec = 0.00087196
-    self.yc = 0.0126
-    self.track = 0.561975
-    self.dt = 1.0/50.0
-    
-    # Kalman model Jacobian
-    self.A = zeros((6,6))
-    
-    # Kalman sensor Jacobian
-    self.H = zeros((2,6))
-    
-    # Kalman process noise
-    q = array((1e-6, 1e-6, 1e-6, 10, 10, 1e-10))
-    self.Q = diagflat(q)
     
     self.firstRun = 1
+    self.firstOdom = 1
     self.trans = 0
     
-    rospy.init_node('cwru_ekf')
-    rospy.Subscriber('crio_sensors', cRIOSensors, self.update_filter)
-    self.odom_pub = rospy.Publisher('odom_ekf', Odometry)
-    self.tf_br = tf.TransformBroadcaster()
-    self.listener = tf.TransformListener()
-    rospy.spin()
+    if(self.publish == 1):
+      rospy.init_node('cwru_ekf')
+      rospy.Subscriber('crio_sensors', cRIOSensors, self.update_filter)
+      self.odom_pub = rospy.Publisher('odom_ekf', Odometry)
+      self.tf_br = tf.TransformBroadcaster()
+      self.listener = tf.TransformListener()
+      rospy.spin()
 	      
   def update_filter(self, msg):
     self.modelJacobian()
-    self.prediction()
+    self.prediction(msg)
     
     self.sensor_update(msg)
-    self.publish_filter()
     
-  def prediction(self):
+    if(self.publish == 1):
+      self.publish_filter()
+      
+    
+  def prediction(self, msg):
     # Model prediction
     x = self.x[0]
     y = self.x[1]
@@ -79,9 +140,17 @@ class EKF:
     self.x[0] = x + v*self.dt*cos(th)
     self.x[1] = y + v*self.dt*sin(th)
     self.x[2] = th + w*self.dt
+    #self.x[3] = venc
+    #self.x[4] = wenc
     
     # Covariance prediction
     self.P = dot(dot(self.A,self.P),self.A.T) + self.Q
+    self.P[0,5] = 0.0
+    self.P[5,0] = 0.0
+    self.P[1,5] = 0.0
+    self.P[5,1] = 0.0
+    self.P[2,5] = 0.0
+    self.P[5,2] = 0.0
     
   def modelJacobian(self):
     th = self.x[2]
@@ -91,6 +160,8 @@ class EKF:
     self.A = eye(6)
     self.A[0,2] = -v*dt*sin(th)
     self.A[0,3] = dt*cos(th)
+    self.A[1,2] = v*dt*cos(th)
+    self.A[1,3] = dt*sin(th)
     self.A[2,4] = dt
     
   def sensor_update(self, msg):
@@ -103,74 +174,96 @@ class EKF:
       self.firstRun = 0
     else:
       
+      # For Enc
       dle = self.lec*(msg.left_wheel_encoder-self.le)
       dre = self.rec*(msg.right_wheel_encoder-self.re)
-      yv = (msg.yaw_rate/1000.0-self.x[5])/self.yc*3.1415/180.0
+      
+      #venc = (dle + dre)/(2.0*self.dt)
+      #wenc = (dle - dre)/(self.track*self.dt)
+      
+      yv = (msg.yaw_rate/1000.0)/self.yc*3.1415/180.0
       
       z = array([dle, dre, yv]).T
       
       self.le = msg.left_wheel_encoder
       self.re = msg.right_wheel_encoder
       
-      lev = self.variance_from_measurement(dle, 1e-12, 0.0002, 0.0)
-      rev = self.variance_from_measurement(dre, 1e-12, 0.0002, 0.0)
-      yawVar = self.variance_from_measurement(yv, .00000019, .000048345, 0.0)
       
-      self.R = diagflat(array([lev, rev, yawVar]))
+      lev = self.variance_from_measurement(dle, self.encV[0], self.encV[1], self.encV[2])
+      rev = self.variance_from_measurement(dre, self.encV[0], self.encV[1], self.encV[2])
+      yawVar = self.variance_from_measurement(yv, self.yawV[0], self.yawV[1], self.yawV[2])
+      
+      R = diagflat(array([lev, rev, yawVar]))
       
       y = z - self.sensor_prediction()
-      S = dot(dot(self.H,self.P),self.H.T) + self.R
+      
+      #print "w: %.3f enc: %.3f yaw: %.3f yawp: %.3f b: %.3f" % (self.x[4], wenc, yv, self.x[4]+self.x[5], self.x[5])
+      
+      #print "w: %s   b: %s    yaw: %s" % (self.x[4], self.x[5], yv)
+      
+      S = dot(dot(self.H,self.P),self.H.T) + R
       K = dot(dot(self.P,self.H.T),linalg.pinv(S))
       self.x = self.x + dot(K,y.T)
       self.P = dot(eye(6)-dot(K,self.H),self.P)
+            
     
   def sensor_prediction(self):
     v = self.x[3]
     w = self.x[4]
+    b = self.x[5]
+    y = w + b
     le = 1.0/2.0*self.dt*(2.0*v+self.track*w)
     re = 1.0/2.0*self.dt*(2.0*v-self.track*w)
-    z = array([le, re, w])
+    z = array([le, re, y])
     return z.T
     
   @staticmethod
   def variance_from_measurement(meas, c0, c1, c2):
-    return c0 + c1 * meas * meas + c2 * meas * meas * meas * meas
+    return c0 + c1 * abs(meas) + c2 * pow(meas,2)
     
   def sensorJacobian(self):
     dt = self.dt
     self.H = zeros((3,6))
     self.H[0,3] = dt
     self.H[1,3] = dt
-    self.H[0,4] = dt/2.0
-    self.H[1,4] = -dt/2.0
+    self.H[0,4] = self.track*dt/2.0
+    self.H[1,4] = -self.track*dt/2.0
     self.H[2,4] = 1.0
     self.H[2,5] = 1.0
     
-  def publish_filter(self):
-    x = self.x[0]
-    y = -self.x[1] # Change to ROS
-    th = -self.x[2] # Change to ROS
-    v = self.x[3]
-    w = -self.x[4] # Change to ROS
+  def getState(self):
+    return self.x
     
+  def getCovariance(self):
+    return self.P
+    
+  def publish_filter(self):    
     if(self.trans == 0):
       try:
-	(self.trans,self.rot) = self.listener.lookupTransform('/map', '/base_link', rospy.Time(0))
-	#(self.trans2,self.rot2) = self.listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
-	#self.trans = self.trans + self.trans2
-	self.tf_br.sendTransform(translation = self.trans, 
+	(self.trans,self.rot) = self.listener.lookupTransform('/map', '/odom', rospy.Time(0))
+	(trans2,rot2) = self.listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
+	self.tf_br.sendTransform(translation = self.trans,
 		  rotation = self.rot,
 		  time = rospy.Time.now(),
 		  child = 'odom_ekf',
 		  parent = 'map')
+	self.x[2] -= tf.transformations.euler_from_quaternion(rot2)[2]
+	self.x[0] += trans2[0]
+	self.x[1] -= trans2[1]
       except (tf.LookupException, tf.ConnectivityException):
 	a = 0
-    else:
+    else: # Copy the transform from amcl to odom
       self.tf_br.sendTransform(translation = self.trans, 
 		rotation = self.rot,
 		time = rospy.Time.now(),
 		child = 'odom_ekf',
 		parent = 'map')
+		
+    x = self.x[0]
+    y = -self.x[1] # Change to ROS
+    th = -self.x[2] # Change to ROS
+    v = self.x[3]
+    w = -self.x[4] # Change to ROS
     
     current_time = rospy.Time.now()
     quaternion = tf.transformations.quaternion_about_axis(th, (0,0,1))
@@ -204,6 +297,6 @@ class EKF:
 
 if __name__ == '__main__':
 	try:
-	  myEKF = EKF()
+	  myEKF = EKF(None)
 
 	except rospy.ROSInterruptException: pass
--- cwru_experimental/cwru_kalibrater/src/cwru_ekf_kalibrater.py
+++ cwru_experimental/cwru_kalibrater/src/cwru_ekf_kalibrater.py
@@ -28,79 +28,152 @@ from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import TransformStamped
 import geometry_msgs.msg
 from cwru_base.msg import cRIOSensors
-from cwru_ekf_dynamic import EKF
+#from cwru_ekf_dynamic import EKF
 from scipy.optimize import fmin
 from scipy.optimize import fmin_bfgs
 from scipy.optimize import fmin_powell
 
 import sys
 import rosbag
+import pylab
 
 class KF_Tuner:
   def __init__(self, bagPath):
-    # gX is: qV qW qB encVc encVl yawVc yawVl lec rec yc track
-    self.bag = rosbag.Bag(bagPath)
-    self.mag = (20, 20, 1, 1, 1, 1, 1)#, 1e-5, 1e-5, .0126, .56)
-    self.signed = (1, 1, 1, 1, -1, 1, -1)#, 1e-5, 1e-5, .0126, .56)
-    #gX = (1.0, 1.0, 1e-6, .002, .002, .002, .002)#, 1e-5, 1e-5, .0126, .56)
-    glX = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)#, 1e-5, 1e-5, .0126, .56)
-    gXopt = fmin(self.newEKF, glX, xtol=1e-6)#fmin_bfgs(self.newEKF, glX)#
-    self.bag.close()
+    # gX is: qV qW qB encVc encVl yawVc yawVl lec rec lmec rmec yc track
+    self.bagsf = []
+    #self.bagsf.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/straight.bag"))
+    #self.bagsf.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/fishtail.bag"))
+    #self.bagsf.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/loop.bag"))
+    #self.bagsf.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/u.bag"))
+    #self.bagsf.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/l.bag"))
+    #self.bags.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/spin.bag"))
+    #self.bags.append(rosbag.Bag("/home/chad/cwru-ros-pkg/cwru_experimental/cwru_kalibrater/src/logs/spin2.bag"))
+    #self.names = []
+    #self.names.append("straight")
+    #self.names.append("fishtail")
+    #self.names.append("loop")
+    #self.names.append("u")
+    #self.names.append("l")
+    '''for i in range(len(self.bagsf)):
+      self.bags = []
+      for j in range(len(self.bagsf)):
+	#if(i != j):
+	self.bags.append(self.bagsf[i])'''
+    self.bagsf.append(rosbag.Bag(bagPath))
+    self.bags = self.bagsf
+    self.tune = (True, True, True, True, False, True, False, True, True, False, False, True, False)
+    self.default = [1.0, 1.0, 1e-6, .01, 0.0, .01, 0.0, 0.00087589, 0.00087196, 0.000058393, 0.000058131, 0.0126276, 0.561975]
+    glX = (1.0, 1.0, 1e-6, .01, .01, 0.00087589, 0.00087196, 0.0126276)
+    gXopt = fmin(self.newEKF, glX)
+    print "----------------------------------"
+    print gXopt
+    self.bags = [self.bagsf[i]]
+    print self.newEKF(gXopt)
+    for bag in self.bagsf:
+      bag.close()
     
   def newEKF(self, glX):
-    gX = glX
-    for i in range(len(glX)):
-      if(self.signed[i] > 0):
-	gX[i] = self.mag[i]*self.logsig(glX[i])
-      else:
-	gX[i] = self.mag[i]*self.signlogsig(glX[i])
+    print glX
+    gX = self.default
+    j = 0
+    for i in range(len(gX)):
+      if(self.tune[i]):
+	gX[i] = glX[j]
+	j = j + 1
     
-    map2odomtrans = False
-    map2odomrot = False
-    odom2basetrans = False
-    odom2baserot = False
-    odomPos = False
-    amclPos = False
-    amclRot = False
-    state = [False]
+
     rmsSum = 0.0
+    pSum = 0.0
     T = 0
-    print gX
-    thisEKF = EKF(gX)
-    for topic, msg, t in self.bag.read_messages(topics=['/odom', '/crio_sensors', '/tf']):
-      if(topic == "/odom"):
-	if(odom2basetrans == False):
-	  odom2basetrans = msg.pose.pose.position
-	  quat = msg.pose.pose.orientation
-	  quat = [quat.x, quat.y, quat.z, quat.w]
-	  odom2baserot = tf.transformations.euler_from_quaternion(quat)[2]
-	odomPos = msg.pose.pose.position
-      elif(topic == "/tf"):
-	tfStamp = msg.transforms[0]
-	frameid = tfStamp.header.frame_id
-	childid = tfStamp.child_frame_id
-	if(frameid == '/map' and childid == "/odom"): # This is an AMCL update grab it!
-	  amclPos = tfStamp.transform.translation
-	  quat = tfStamp.transform.rotation
-	  quat = [quat.x, quat.y, quat.z, quat.w]
-	  amclRot = tf.transformations.euler_from_quaternion(quat)[2]
-      elif(topic == "/crio_sensors"):
-	# Run EKF update and add to RMS
-	thisEKF.update_filter(msg)
-	state = thisEKF.getState()
-	# Figure out actual position
-	if(odom2basetrans != False and odomPos != False and state[0] != False and amclPos != False):
-	  # Transform odom to map to get actual
-	  actual = self.transformFrame(odomPos.x, odomPos.y, amclPos, amclRot)
-	  
-	  # Transform EKF into map to get estimated
-	  kfOdom = self.transformFrame(state[0], state[1], odom2basetrans, odom2baserot)
-	  kfMap = self.transformFrame(kfOdom[0], kfOdom[1], amclPos, amclRot)
-	  
-	  rmsSum += math.pow(actual[0]-kfMap[0],2) + math.pow(actual[1]-kfMap[1],2)
-	  T += 1
+    C = 0
+
+    for b in range(len(self.bags)):
+      curBag = self.bags[b]
+      map2odomtrans = False
+      map2odomrot = False
+      odom2basetrans = False
+      odom2baserot = False
+      odomPos = False
+      amclPos = False
+      amclRot = False
+      amclInitPos = False
+      amclInitRot = False
+      state = zeros((6,1))
+      state[0] = False
+      #print gX
+      thisEKF = EKF(gX)
+      actualList = []
+      ekfList = []
+      for topic, msg, t in curBag.read_messages(topics=['/odom', '/crio_sensors', '/tf']):
+	if(topic == "/odom"):
+	  odomPos = msg.pose.pose.position
+	  if(odom2basetrans == False):
+	    odom2basetrans = msg.pose.pose.position
+	    quat = msg.pose.pose.orientation
+	    quat = [quat.x, quat.y, quat.z, quat.w]
+	    odom2baserot = tf.transformations.euler_from_quaternion(quat)[2]
+	elif(topic == "/tf"):
+	  tfStamp = msg.transforms[0]
+	  frameid = tfStamp.header.frame_id
+	  childid = tfStamp.child_frame_id
+	  if(frameid == '/map' and childid == "/odom"): # This is an AMCL update grab it!
+	    amclPos = tfStamp.transform.translation
+	    quat = tfStamp.transform.rotation
+	    quat = [quat.x, quat.y, quat.z, quat.w]
+	    amclRot = tf.transformations.euler_from_quaternion(quat)[2]
+	    if(amclInitPos == False):
+	      amclInitPos = amclPos
+	      amclInitRot = amclRot
+	elif(topic == "/crio_sensors"):
+	  # Run EKF update and add to RMS
+	  thisEKF.update_filter(msg)
+	  state = thisEKF.getState()
+	  covar = thisEKF.getCovariance()
+	  # Figure out actual position
+	  if(odom2basetrans != False and odomPos != False and state[0] != False and amclPos != False):
+	    # Transform odom to map to get actual
+	    actual = array(self.transformFrame(odomPos.x, odomPos.y, amclPos, amclRot))
+	    #print actual
+	    
+	    # Transform EKF into map to get estimated
+	    kfOdom = self.transformFrame(state[0], -state[1], odom2basetrans, odom2baserot)
+	    kfMap = array(self.transformFrame(kfOdom[0], kfOdom[1], amclInitPos, amclInitRot))
+	    
+	    #print kfMap
+	    actualList.append([actual[0],actual[1]])
+	    ekfList.append([kfMap[0],kfMap[1]])
+	    
+	    try:
+	      rmsSum += math.pow(actual[0]-kfMap[0],2) + math.pow(actual[1]-kfMap[1],2)
+	      H = zeros((2,6))
+	      H[0,0] = 1.0
+	      H[1,1] = 1.0
+	      omega = dot(H,dot(covar,H.T))
+	      Ps = log(2.0*3.14159*omega)+dot(dot((actual-kfMap).T,linalg.pinv(omega)),(actual-kfMap))
+	      pSum = (Ps[0,0] + Ps[1,1])
+	      T += 1
+	    except:
+	      rmsSum += 1e6
+	      pSum += 1e6
+	      T += 1
+	      
+      try:
+	# Plot
+	actualList = array(actualList)
+	ekfList = array(ekfList)
+	pylab.plot(actualList[:,0], actualList[:,1], 'g', ekfList[:,0], ekfList[:,1], 'b')
+	pylab.xlabel('x coord (m)')
+	pylab.ylabel('y coord (m)')
+	pylab.savefig('currentPaths'+self.names[b])
+	pylab.clf()
+      except:
+	pylab.clf()
+	    
     rmsErr = math.sqrt(1.0/float(T)*rmsSum)
     print rmsErr
+    #print pSum
+    
+    #raw_input()
     return rmsErr
   
   def transformFrame(self, x, y, trans, th):
@@ -108,7 +181,7 @@ class KF_Tuner:
     yp = x*sin(th)+y*cos(th)+trans.y
     return [xp, yp]
     
-  def logsig(self,x):
+  '''def logsig(self,x):
     if(x > 100):
       return 1.0
     elif(x < -100):
@@ -125,7 +198,7 @@ class KF_Tuner:
       return 2.0 / (1.0 + exp(-x))-1.0
     
   def tansig(self, x):
-    return 2.0/(1.0+exp(-2.0*x))-1.0
+    return 2.0/(1.0+exp(-2.0*x))-1.0'''
 
 if __name__ == '__main__':
 	try:
@@ -133,5 +206,11 @@ if __name__ == '__main__':
 	    bagPath = sys.argv[1]
 	  except IndexError:
 	    sys.exit("ERROR: You did not specify the bag location!")
-	  myKFT = KF_Tuner(bagPath)
+	  try:
+	    EKFFile = sys.argv[2].split(".py")[0]
+	    im_string = "from " + EKFFile + " import EKF"
+	    exec im_string
+	  except IndexError:
+	    sys.exit("ERROR: You did not specify which EKF to run!")
+	  myKFT = KF_Tuner("")
 	except rospy.ROSInterruptException: pass
