6beaacec8a52c0add092cf73a7f71cc553d57a4d
Roberto <roberto@roberto.(none)>
2011-10-10 16:48:53 -0400

    picking random points inside bounding box map.

--- cwru_tour_guide/tour_guide_executive/src/test1.py
+++ cwru_tour_guide/tour_guide_executive/src/test1.py
@@ -5,7 +5,8 @@ import roslib
 roslib.load_manifest('tour_guide_executive')
 import rospy
 
-
+import math
+import random
 import time
 import sys
 import vlc
@@ -20,8 +21,6 @@ from actionlib_msgs.msg import GoalStatus
 
 import yaml
 
-sound_prefix=''
-
 def create_move_base_goal_from_yaml(yaml_goal):
     """Creates a MoveBaseGoal from a goal loaded up from the yaml file"""
     goal = MoveBaseGoal()
@@ -35,81 +34,52 @@ def create_move_base_goal_from_yaml(yaml_goal):
     
     return goal
 
+def create_move_base_goal_at_random():
 
-#define an empty stall function for now
-def stall():  
-  return 'testsound1.wav'
+    MAX_X = 10
+    MIN_X = 1
+    MAX_Y = 10
+    MIN_Y = 1
 
-def main(filename):
-  soundpath=rospy.get_param('~soundpath')
-  
-  #open the yaml config file and load path and tour
-  with open(filename, 'r') as datafile:
-    data = yaml.safe_load(datafile)
-  jokelist=data['jokes']
-  goallist=data['goals']
-  for entry in goallist:
-    print entry
-  p=0
-  i=vlc.Instance()
-  iterator=goallist.__iter__();
+    goal = MoveBaseGoal()
+
+    goal.target_pose.header.frame_id = '/map'
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    goal.target_pose.pose.position.x = random.uniform(MIN_X, MAX_X)
+    goal.target_pose.pose.position.y = random.uniform(MIN_Y, MAX_Y)
+    theta = random.uniform(0,2*math.pi)
+    quaternion = tf.transformations.quaternion_about_axis(theta, (0,0,1))
+    goal.target_pose.pose.orientation = Quaternion(*quaternion)
+
+    return goal
+    
+def main():
   client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
   client.wait_for_server()
   while(1):
     try:
+      import ipdb; ipdb.set_trace()
+
       print 'starting location'
-      nextThing=iterator.next()
-      goal=create_move_base_goal_from_yaml(nextThing['goal'])
-      sound=nextThing['wavs']
+      goal=create_move_base_goal_at_random()
       print 'send location'
       client.send_goal(goal)
-      #client.wait_for_result()
-      print 'got next file'
-      if p!=0:
-        p.release()
-
-      p=vlc.MediaPlayer(sound_prefix + sound[0])
-      print 'created next file'
-      
-      print 'retained next file'
-      p.play()
-      print(p.get_state())
+      client.wait_for_result()
       
       while(1):
- #       print 'starting inner loop'
-        while(p.get_state()!=vlc.State.Ended):
- #         print(p.get_state())
-          time.sleep(.2)
-
-        time.sleep(.7)
-
-        print 'finished sound'
-        if client.get_state() == GoalStatus.SUCCEEDED :
-          break
-        elif client.get_state()==GoalStatus.ABORTED or client.get_state()==GoalStatus.REJECTED :
-          #resend the goal
-          client.send_goal(goal)
+	current_state = client.get_state()
+        if current_state == GoalStatus.SUCCEEDED or current_state == GoalStatus.ABORTED or current_state == GoalStatus.REJECTED :
+            break
         else:
- #         print 'starting stall sound'
-          time.sleep(.7)
-       #   p=vlc.MediaPlayer(sound_prefix + stall())
-       #   p.play()
-        #if client.get_state() == GoalStatus.SUCCEEDED:
-	    #rospy.loginfo("Goal executed successfully")
-	#else:
-	    #rospy.logerr("Could not execute goal for some reason")
-      
+            time.sleep(.7)
+            print "status: %s" % current_state
+            
     except StopIteration:
       break;
 
   rospy.sleep(2)
 
-  #time.sleep(5);
-
 if __name__ == '__main__':
   rospy.init_node('roberto_goal_planner')
-  sound_prefix=rospy.get_param('~soundpath')
-  print('woooow'+sound_prefix)
-  
-  main(rospy.get_param('~filename'))
-  
+  main()
