ce8df586bd2ee0e9d1e225fa700977fe8b4f0b68
roberto <roberto@roberto.(none)>
2012-06-26 15:29:23 -0400

    This file will bring up bk_nav

---
+++ cwru_tour_guide/roberto_bk_nav_launch.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+export ROBOT=roberto
+
+roslaunch `rosstack find cwru_semi_stable`/cwru_bringup_no_tele.launch &
+
+sleep 5
+roslaunch `rospack find cwru_nav`/start_tour_guide_no_nav.launch &
+
+sleep 1
+roslaunch rviz.launch &
+
+sleep 5
+
+roslaunch `rospack find tour_guide_executive`/seed_initial_pose.launch &
+
+sleep 5
+roslaunch `rospack find tour_guide_executive`/start_executive.launch &
+
+wait
+
