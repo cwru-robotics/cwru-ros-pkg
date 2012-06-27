f74b5149adecc164933efbca11b1d6a31ec1ead6
Bill Kulp <bk8190@gmail.com>
2012-06-27 19:14:33 -0400

    Updated initial pose seeding

--- cwru_tour_guide/tour_guide_executive/src/initial_pose_setup.py
+++ cwru_tour_guide/tour_guide_executive/src/initial_pose_setup.py
@@ -15,16 +15,15 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 def main(pub):
   initialPose=PoseWithCovarianceStamped()
   initialPose.header.frame_id='/map'
-  initialPose.pose.pose.position.x=-5.49
-  initialPose.pose.pose.position.y=32.45
+  initialPose.pose.pose.position.x=-20.23
+  initialPose.pose.pose.position.y=-16.41
   
   pi = 3.141
-  quaternion = tf.transformations.quaternion_about_axis(19*pi/16 , (0,0,1))
+  quaternion = tf.transformations.quaternion_about_axis(pi*1.2 , (0,0,1))
   initialPose.pose.pose.orientation= Quaternion(*quaternion)
   initialPose.pose.covariance[0]=.25
   initialPose.pose.covariance[7]=.25
   initialPose.pose.covariance[35]=.0685
-  #pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped)
 
   pub.publish(initialPose)
   rospy.sleep(.5)
