f665acd236b6ce3916e8ea2196111a0cdbaa990b
Ed Venator <esv@case.edu>
2012-12-12 19:42:54 -0500

    Added a joint state publisher to the gripper.
    The rosserial node on the arduino now publishes the joint state
    for both gripper jaws every 50ms. This has been shown to compile,
    but has not yet been run on an arduino or tested on the robot.

--- cwru_abby/abby_gripper/src/gripper/gripper.ino
+++ cwru_abby/abby_gripper/src/gripper/gripper.ino
@@ -8,10 +8,16 @@
 #include <ros.h>
 #include <ros/time.h>
 #include <abby_gripper/gripper.h>
+#include <sensor_msgs/JointState.h>
 
 ros::NodeHandle  nh;
+sensor_msgs::JointState joint_msg;
+ros::Publisher joint_pub("joint_states", &joint_msg);
 const int gripper_pin = 13;
 boolean position = LOW;
+
+#define CLOSED_POSITION -0.023
+#define OPEN_POSITION -0.031
 
 void callback(const abby_gripper::gripperRequest &req, abby_gripper::gripperResponse &resp){
   // Set the gripper output to high for open and low for closed
@@ -36,11 +42,30 @@ ros::ServiceServer<abby_gripper::gripperRequest, abby_gripper::gripperResponse> 
 
 void setup(){
   pinMode(gripper_pin,OUTPUT);
+  
+  joint_msg.name[0] = "gripper_jaw_1";
+  joint_msg.name[1] = "gripper_jaw_2";
+  joint_msg.velocity[0] = 0.0;
+  joint_msg.velocity[1] = 0.0;
+  joint_msg.effort[0] = 0.0;
+  joint_msg.effort[1] = 0.0;
+  
   nh.initNode();
   nh.advertiseService(server);
+  nh.advertise(joint_pub);
 }
 
 void loop()
 {
   nh.spinOnce();
+  if(position){
+      joint_msg.position[0] = OPEN_POSITION;
+      joint_msg.position[1] = OPEN_POSITION;
+  }
+  else{
+      joint_msg.position[0] = CLOSED_POSITION;
+      joint_msg.position[1] = CLOSED_POSITION;
+  }
+  joint_pub.publish(&joint_msg);
+  delay(50);
 }
