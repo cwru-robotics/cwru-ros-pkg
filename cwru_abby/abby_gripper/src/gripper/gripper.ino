82c8c72e93a67f442ba8ed68ea293d9650eb2c3f
Ed Venator <esv@case.edu>
2012-12-14 12:31:07 -0500

    Fixed a typo in the gripper description.

--- cwru_abby/abby_gripper/src/gripper/gripper.ino
+++ cwru_abby/abby_gripper/src/gripper/gripper.ino
@@ -11,19 +11,18 @@
 #include <sensor_msgs/JointState.h>
 
 ros::NodeHandle  nh;
-sensor_msgs::JointState joint_msg;
-ros::Publisher joint_pub("joint_states", &joint_msg);
 const int gripper_pin = 13;
 boolean position = LOW;
 
 #define CLOSED_POSITION -0.03
 #define OPEN_POSITION -0.039
 
-float velocities[2] = {0.0, 0.0};
-float efforts[2] = {0.0, 0.0};
-float positions[2] = {OPEN_POSITION, OPEN_POSITION};
-char joint_0[14] = "gripper_jaw_1";
-char joint_1[14] = "gripper_jaw_2";
+sensor_msgs::JointState joint_msg;
+ros::Publisher joint_pub("joint_states", &joint_msg);
+float joint_velocities[2] = {0.0, 0.0};
+float joint_efforts[2] = {0.0, 0.0};
+float joint_positions[2] = {OPEN_POSITION, OPEN_POSITION};
+char * joint_names[14] = {"gripper_jaw_1", "gripper_jaw_2"};
 
 void callback(const abby_gripper::gripperRequest &req, abby_gripper::gripperResponse &resp){
   // Set the gripper output to high for open and low for closed
@@ -48,12 +47,12 @@ ros::ServiceServer<abby_gripper::gripperRequest, abby_gripper::gripperResponse> 
 
 void setup(){
   pinMode(gripper_pin,OUTPUT);
+  
   joint_msg.name_length = joint_msg.velocity_length = joint_msg.effort_length = joint_msg.position_length = 2;
-  joint_msg.name[0] = (char*) &joint_0;
-  joint_msg.name[1] = (char*) &joint_1;
-  joint_msg.velocity = velocities;
-  joint_msg.effort = efforts;
-  joint_msg.position = positions;
+  joint_msg.name = joint_names;
+  joint_msg.velocity = joint_velocities;
+  joint_msg.effort = joint_efforts;
+  joint_msg.position = joint_positions;
   
   nh.initNode();
   nh.advertiseService(server);
@@ -63,12 +62,12 @@ void setup(){
 void loop()
 {
   if(position){
-      positions[0] = OPEN_POSITION;
-      positions[1] = OPEN_POSITION;
+      joint_positions[0] = OPEN_POSITION;
+      joint_positions[1] = OPEN_POSITION;
   }
   else{
-      positions[0] = CLOSED_POSITION;
-      positions[1] = CLOSED_POSITION;
+      joint_positions[0] = CLOSED_POSITION;
+      joint_positions[1] = CLOSED_POSITION;
   }
   joint_pub.publish(&joint_msg);
   nh.spinOnce();
