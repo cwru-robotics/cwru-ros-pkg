ec32d366b9fb63945cdb3520665cd582d432579b
Abby Robot <esv@case.edu>
2013-01-31 11:36:06 -0500

    Fixes store object coordinates.
    These coordinates are tested/working.

--- cwru_abby/abby_arm_actions/src/store_object.py
+++ cwru_abby/abby_arm_actions/src/store_object.py
@@ -25,9 +25,12 @@ if __name__ == '__main__':
     pos_constraint.header.frame_id = "/irb_120_base_link"
     pos_constraint.link_name = "gripper_body"
     
-    pos_constraint.position.x = 0.166
-    pos_constraint.position.y = -0.118
-    pos_constraint.position.z = -0.1862
+    #pos_constraint.position.x = 0.0991673
+    #pos_constraint.position.y = 0.406008
+    #pos_constraint.position.z = 0.0102208
+    pos_constraint.position.x = 0.0816875
+    pos_constraint.position.y = 0.207909
+    pos_constraint.position.z = 0.243964
     pos_constraint.constraint_region_shape.type = Shape.BOX
     pos_constraint.constraint_region_shape.dimensions = [0.05, 0.05, 0.05]
     pos_constraint.constraint_region_orientation.x = 0;
@@ -44,10 +47,10 @@ if __name__ == '__main__':
     o_constraint.header = pos_constraint.header
     o_constraint.link_name = pos_constraint.link_name
     
-    o_constraint.orientation.x = 0.6655
-    o_constraint.orientation.y = -0.2585
-    o_constraint.orientation.z = -0.2805
-    o_constraint.orientation.w = -0.6415
+    o_constraint.orientation.x =  0.847531
+    o_constraint.orientation.y =  0.297557
+    o_constraint.orientation.z = -0.140450
+    o_constraint.orientation.w =  0.416442
     o_constraint.absolute_roll_tolerance = 0.04
     o_constraint.absolute_pitch_tolerance = 0.04
     o_constraint.absolute_yaw_tolerance = 0.04
