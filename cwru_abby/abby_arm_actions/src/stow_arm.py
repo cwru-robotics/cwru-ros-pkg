4205c12685c3dc5aff10c708db227148d409cb36
Ed Venator <esv@case.edu>
2013-02-02 00:52:30 -0500

    OOP'd the arm actions.
    Stow arm is now a class with a main method that runs the class's
    method to stow the arm.
    
    Store object is now a class with a main method that runs the class's
    method to drop an object in the bin. Also added the gripper to the
    store object method.
    
    Neither of these are yet tested on the robot, but both are syntactically
    good.

--- cwru_abby/abby_arm_actions/src/stow_arm.py
+++ cwru_abby/abby_arm_actions/src/stow_arm.py
@@ -5,47 +5,63 @@ import rospy
 import actionlib
 from arm_navigation_msgs.msg import *
 
+class StowArm:
+    def __init(self):
+        self.client = actionlib.SimpleActionClient('move_irb_120', MoveArmAction)
+        
+        rospy.logdebug("Setting up goal request message.")
+        self.goal = MoveArmGoal()
+        self.goal.planner_service_name = "/ompl_planning/plan_kinematic_path"
+        
+        motion_plan_request = MotionPlanRequest()
+        motion_plan_request.group_name = "irb_120"
+        motion_plan_request.num_planning_attempts = 1
+        motion_plan_request.planner_id = ""
+        motion_plan_request.allowed_planning_time = rospy.Duration(5,0)
+        for i in range(0,6):
+            joint_constraint = JointConstraint()
+            joint_constraint.joint_name = "joint"+str(i+1)
+            joint_constraint.tolerance_below = 0.05
+            joint_constraint.tolerance_above = 0.05
+            motion_plan_request.goal_constraints.joint_constraints.append(joint_constraint)
+        #Joint Positions
+        motion_plan_request.goal_constraints.joint_constraints[0].position = 0.00
+        motion_plan_request.goal_constraints.joint_constraints[1].position = -1.57
+        motion_plan_request.goal_constraints.joint_constraints[2].position = 1.22
+        motion_plan_request.goal_constraints.joint_constraints[3].position = 0.0
+        motion_plan_request.goal_constraints.joint_constraints[4].position = 0.03
+        motion_plan_request.goal_constraints.joint_constraints[5].position = -0.84
+        
+        self.goal.motion_plan_request = motion_plan_request
+        
+        rospy.loginfo("Waiting for action server...")
+        client.wait_for_server()
+        rospy.loginfo("Connected to action server.")
+        
+    def sendOnce(self, timeOut = 60):
+        rospy.loginfo('Sending stow goal...')
+        self.client.send_goal(self.goal)
+        if client.wait_for_result(rospy.Duration(timeOut, 0)):
+            return client.get_result()
+        else:
+            rospy.logwarn('Timed out attempting to stow arm.')
+            return False
+    
+    def sendUntilSuccess(self, timeOut = 60):
+        result = False
+        r = rospy.Rate(1)
+        while not result:
+            result = self.sendOnce(timeOut)
+            if not result:
+                r.sleep()
+        status = result.error_code
+        if status == status.SUCCESS:
+            rospy.loginfo("Arm successfully stowed.")
+        else:
+            rospy.logwarn("Arm failed to stow.")
+
 if __name__ == '__main__':
     rospy.init_node('stow_arm')
     rospy.loginfo("Node initialized.")
-    client = actionlib.SimpleActionClient('move_irb_120', MoveArmAction)
-    rospy.loginfo("Waiting for action server...")
-    client.wait_for_server()
-    rospy.loginfo("Connected to action server.")
-    
-    goal = MoveArmGoal()
-    goal.motion_plan_request.group_name = "irb_120"
-    goal.motion_plan_request.num_planning_attempts = 1
-    goal.motion_plan_request.planner_id = ""
-    goal.planner_service_name = "/ompl_planning/plan_kinematic_path"
-    goal.motion_plan_request.allowed_planning_time = rospy.Duration(5,0)
-    for i in range(0,6):
-    	joint_constraint = JointConstraint()
-    	joint_constraint.joint_name = "joint"+str(i+1)
-    	goal.motion_plan_request.goal_constraints.joint_constraints.append(joint_constraint)
-        goal.motion_plan_request.goal_constraints.joint_constraints[i].tolerance_below = 0.05
-        goal.motion_plan_request.goal_constraints.joint_constraints[i].tolerance_above = 0.05
-        
-    #Joint Positions
-    goal.motion_plan_request.goal_constraints.joint_constraints[0].position = 0.00
-    goal.motion_plan_request.goal_constraints.joint_constraints[1].position = -1.57
-    goal.motion_plan_request.goal_constraints.joint_constraints[2].position = 1.22
-    goal.motion_plan_request.goal_constraints.joint_constraints[3].position = 0.0
-    goal.motion_plan_request.goal_constraints.joint_constraints[4].position = 0.03
-    goal.motion_plan_request.goal_constraints.joint_constraints[5].position = -0.84
-    
-    rospy.loginfo("Sending stow goal...")
-    client.send_goal(goal)
-    finishedInTime = client.wait_for_result(rospy.Duration(60,0))
-    r= rospy.Rate(0.1);
-    while not finishedInTime:
-        client.cancel_goal()
-        rospy.loginfo("Timed out trying to stow arm. Resending stow goal...")
-        client.send_goal(goal)
-        finishedInTime = client.wait_for_result(rospy.Duration(60,0))
-        r.sleep();
-    status = client.get_result().error_code.val
-    if status == ArmNavigationErrorCodes.SUCCESS:
-        rospy.loginfo("Arm successfully stowed.")
-    else:
-        rospy.logwarn("Arm failed to stow.")
+    stowArm = StowArm()
+    stowArm.sendUntilSuccess()
